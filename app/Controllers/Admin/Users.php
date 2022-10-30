27e6a1854b9e95179a5980c7a3d95dfc5e5d8892
Trie Hanoto Wibowo <triehanotowibowo@gmail.com>
2022-10-30 20:43:59 +0700

    routes & controller

--- app/Controllers/Admin/Users.php
+++ app/Controllers/Admin/Users.php
@@ -1,15 +1,17 @@
 <?php
 
-namespace App\Controllers;
+namespace App\Controllers\Admin;
 
-class Coba extends BaseController
+use App\Controllers\BaseController;
+
+class Users extends BaseController
 {
     public function index()
     {
         echo "ini controller coba method index";
     }
 
-    public function About($param = "", $param2 = 0)
+    public function about($param = "", $param2 = 0)
     {
         // echo "My name is $this->nama.";
         echo "My name is $param. umur saya $param2";
